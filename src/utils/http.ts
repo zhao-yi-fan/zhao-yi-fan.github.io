bae4e9221ca85675974bf40aad201058a6637616
zhaoyifan <zhaoyifan@fehorizon.com>
2022-07-05 23:32:31 +0800

    feat: utility type

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -42,10 +42,45 @@ export const http = (
     });
 };
 
+// JS 中的typeof 是在runtime时运行的
+// return typeof 1 ==='number'
 
+// TS 中的typeof 是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
 export const useHttp = () => {
   const { user } = useAuth();
-  // TODO 讲解 TS 操作符
+  // Utility Types 的用法： 用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
 };
+
+// 交叉类型
+// let myFavoriteNumber: string & number;
+// 联合类型
+let myFavoriteNumber: string | number;
+myFavoriteNumber = "seven";
+myFavoriteNumber = 7;
+// 不能将类型“{}”分配给类型“string | number”。ts(2322)
+// myFavoriteNumber = {}
+let jackFavoriteNumber: string | number;
+
+// 类型别名在很多情况下可以和interface互换
+// interface Person {
+//   name: string;
+// }
+// type Person = { name: string };
+// const xiaoMing: Person = { name: "xiaoming" };
+
+// 1、类型别名, interface 在这种情况下没法替代type
+type FavoriteNumber = string | number;
+let roseFavoriteNumber: FavoriteNumber = "6";
+
+//2、 interface 无法实现Utility Types
+type Person = {
+  name: string;
+  age: number;
+};
+// 在不改变原有type下，Partial可以让type中的属性都是可选的
+const xiaoMing: Partial<Person> = {};
+// 在不改变原有type下，Omit可以让type的某些属性删除
+const shenMiRen: Omit<Person, "name" | "age"> = { age: 11 };
