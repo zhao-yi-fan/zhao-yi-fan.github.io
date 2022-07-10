33caa90ed4ea09d476fc07402bea20534a77dcd4
zhaoyifan <993445491@qq.com>
2022-07-10 10:52:02 +0800

    chore: andt

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { loadDevTools } from "jira-dev-tool";
+// 在jira-dev-tool引入为了同时修改样式
+import "antd/dist/antd.less";
 import { AppProviders } from "context";
 
 loadDevTools(() =>
