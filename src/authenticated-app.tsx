2eebc91e19c500fdf169bb34a41b92a34ae5f8e5
zhaoyifan <zhaoyifan@fehorizon.com>
2022-08-20 01:35:19 +0800

    chore: 项目页

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,46 @@
+import styled from "@emotion/styled";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreent } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreent />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreent />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+`;
