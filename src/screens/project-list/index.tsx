d6bd84e6b51837e3c57d74265cabf4315b484b17
zhaoyifan <zhaoyifan@fehorizon.com>
2022-08-26 10:32:28 +0800

    chore: 增加loading error

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,32 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { cleanObject, useDebounce, useMount } from "utils";
-import { List } from "./list";
-import { SearchPanel } from "./search-panel";
-import { useHttp } from "utils/http";
-import styled from "@emotion/styled";
+import React, { useEffect, useState } from 'react';
+import { cleanObject, useDebounce, useMount } from 'utils';
+import { List } from './list';
+import { SearchPanel } from './search-panel';
+import { useHttp } from 'utils/http';
+import styled from '@emotion/styled';
+import { Typography } from 'antd';
 
 export const ProjectListScreent = () => {
   const [param, setParam] = useState({
-    name: "",
-    personId: "",
+    name: '',
+    personId: '',
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<null | Error>(null);
+
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
   const debouncedParam = useDebounce(param, 200);
   const client = useHttp();
 
   useEffect(() => {
-    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setIsLoading(true);
+    client('projects', { data: cleanObject(debouncedParam) })
+      .then(setList)
+      .catch((error) => {
+        setError(error);
+        setList([]);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedParam]);
 
   useMount(() => {
-    client("users").then(setUsers);
+    client('users').then(setUsers);
   });
   return (
     <Container>
       <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
+      {error ? (
+        <Typography.Text type={'danger'}>{error.message}</Typography.Text>
+      ) : null}
+      <List loading={isLoading} users={users} dataSource={list} />
     </Container>
   );
 };
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,7 +1,7 @@
-import { Table } from "antd";
-import dayjs from "dayjs";
-import React from "react";
-import { User } from "./search-panel";
+import { Table, TableProps } from 'antd';
+import dayjs from 'dayjs';
+import React from 'react';
+import { User } from './search-panel';
 
 interface Project {
   id: string;
@@ -11,48 +11,47 @@ interface Project {
   organization: string;
   created: number;
 }
-interface ListProps {
-  list: Project[];
+interface ListProps extends TableProps<Project> {
   users: User[];
 }
-export const List = ({ users, list }: ListProps) => {
+export const List = ({ users, ...props }: ListProps) => {
   return (
     <Table
       columns={[
         {
-          title: "名称",
-          dataIndex: "name",
+          title: '名称',
+          dataIndex: 'name',
           sorter: (a, b) => a.name.localeCompare(b.name),
         },
         {
-          title: "部门",
-          dataIndex: "organization",
+          title: '部门',
+          dataIndex: 'organization',
         },
         {
-          title: "负责人",
+          title: '负责人',
           render: (value, project) => {
             return (
               <span>
                 {users.find((user) => user.id === project.personId)?.name ||
-                  "未知"}
+                  '未知'}
               </span>
             );
           },
         },
         {
-          title: "创建时间",
+          title: '创建时间',
           render: (value, project) => {
             return (
               <span>
                 {project.created
-                  ? dayjs(project.created).format("YYYY-MM-DD")
-                  : ""}
+                  ? dayjs(project.created).format('YYYY-MM-DD')
+                  : ''}
               </span>
             );
           },
         },
       ]}
-      dataSource={list}
+      {...props}
     ></Table>
   );
 };
