cc93633790274e094af895950447a282e446b3ae
zhaoyifan <993445491@qq.com>
2026-01-16 17:46:10 +0800

    fix: 移除 CDN 方式，改回打包方式
    React 19 不再提供官方 UMD 构建，CDN 链接返回 404
    - 移除 index.html 中的 CDN script 标签
    - 移除 craco.config.js 中的 webpack externals 配置
    - 将 react 和 react-dom 移回 dependencies

--- craco.config.js
+++ craco.config.js
@@ -6,15 +6,6 @@ module.exports = {
         publicPath: './'
       }
       
-      // 只在生产环境使用 CDN，开发环境使用本地包
-      if (env === 'production') {
-        // 配置 externals，让 React 和 React-DOM 通过 CDN 引入，不打包进 bundle
-        webpackConfig.externals = {
-          'react': 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-      
       return webpackConfig
     }
   },
