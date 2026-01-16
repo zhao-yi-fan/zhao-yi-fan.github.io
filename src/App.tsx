8cf2bf3bd7b749e68d732c2403503951e2f40a3c
zhaoyifan <993445491@qq.com>
2026-01-16 15:44:36 +0800

    优化项目：添加TypeScript类型定义、修复Card重复title、优化响应式设计和HTML配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Card, List } from 'antd';
 import './App.css';
 
-const data = [
+// 项目数据类型定义
+interface ProjectItem {
+  title: string;
+  link: string;
+  description: string;
+}
+
+const data: ProjectItem[] = [
   {
     title: '笔记doc',
     link: 'https://blog.zhaoyifan.top',
@@ -17,11 +24,11 @@ const data = [
   //   link: './bunny-ui/',
   //   description: 'vue2组件库',
   // },
-  {
-    title: 'bunny-ui-next',
-    link: './bunny-ui-next/',
-    description: 'vue3组件库',
-  },
+  // {
+  //   title: 'bunny-ui-next',
+  //   link: './bunny-ui-next/',
+  //   description: 'vue3组件库',
+  // },
   {
     title: 'jira管理',
     link: './jira/',
@@ -32,23 +39,31 @@ const data = [
     link: './study/',
     description: '总结小栗子',
   },
-  {
-    title: 'netlify serverless',
-    link: 'https://determined-benz-6b3316.netlify.app/',
-    description: 'netlify serverless react17项目',
-  },
+  // {
+  //   title: 'netlify serverless',
+  //   link: 'https://determined-benz-6b3316.netlify.app/',
+  //   description: 'netlify serverless react17项目',
+  // },
 ];
 
 function App() {
   return (
     <div className="App">
       <List
-        grid={{ gutter: 16, column: 4 }}
+        grid={{
+          gutter: 16,
+          xs: 1, // 超小屏幕：1列
+          sm: 2, // 小屏幕：2列
+          md: 3, // 中等屏幕：3列
+          lg: 4, // 大屏幕：4列
+          xl: 4, // 超大屏幕：4列
+          xxl: 4, // 超超大屏幕：4列
+        }}
         dataSource={data}
         renderItem={(item) => (
           <List.Item>
-            <a href={item.link} target={'_blank'} rel="noreferrer">
-              <Card hoverable title={item.title}>
+            <a href={item.link} target="_blank" rel="noreferrer">
+              <Card hoverable>
                 <Card.Meta title={item.title} description={item.description} />
               </Card>
             </a>
